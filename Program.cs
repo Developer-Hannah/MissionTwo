5d59b2dcde4193a7d0c360ab50519cbcf58a1bf2
hccam <hccam@CAM>
2024-01-24 16:10:29 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int numRolls = 0;
+
+        int[] rollTotals = new int[13]; //this line is in second class
+
+        System.Console.Write("Welcome to the dice throwing simulator!" + "\n" + "\n" + "How many dice rolls would you like to simulate? ");
+        numRolls = int.Parse(Console.ReadLine());
+
+        rollTotals = MissionTwo.RollDice.SingleRollDice(numRolls);
+
+        Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS" +
+            $"\nEach \"*\" represents 1% of the total number of rolls." +
+            $"\nTotal number of rolls = 1000.");
+
+        for (int total = 2; total <= 12; total++)
+        {
+            int percentage = rollTotals[total] * 100 / numRolls;
+
+            Console.Write("\n" + total + ": ");
+            for (int i = 0; i < percentage; i++)
+            {
+                Console.Write("*");
+            }
+        }
+
+        Console.WriteLine ("\n\nThank you for using the dice throwing simulator. Goodbye!\r\n");
+
+        //System.Console.WriteLine("DICE ROLLING SIMULATION RESULTS" + "\n" + "Each '*' represents 1% of the total number of rolls." + "\n" + "Total number of rolls = 1000.");
+
+        //System.Console.WriteLine("Thank you for using the dice throwning simulator. Goodbye!");
+
+    }
+}
---
+++ RollDice.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace MissionTwo
+{
+    internal class RollDice
+    {
+        private static Random random = new Random();
+
+        public static int[] SingleRollDice(int numRolls)
+        {
+            int[] rollTotals = new int[13];
+
+            for (int i = 1; i <= numRolls; i++)//for the number of times that the user specified
+            {
+                int die1 = random.Next(1, 7); //random number 1-6
+                int die2 = random.Next(1, 7); //random number 1-6
+                int total = die1 + die2;
+
+
+                rollTotals[total]++;
+            }
+            return rollTotals;
+            //Rolls are done
+
+        }
+    }
+}
